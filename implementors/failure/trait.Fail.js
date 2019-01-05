b20a362d0e3c47e1b0538fa460746269411db0c6
Travis CI User <travis@example.org>
2019-01-05 16:31:44 +0000

    Documentation for bheisler/criterion.rs@b0efca3

---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["criterion_plot"] = [{text:"impl Fail for <a class=\"enum\" href=\"criterion_plot/enum.VersionError.html\" title=\"enum criterion_plot::VersionError\">VersionError</a>",synthetic:false,types:["criterion_plot::VersionError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
